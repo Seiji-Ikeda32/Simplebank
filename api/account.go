b02a465fd86ea84b4e9d987fdafc5d8a08258068
池田聖示 <ikeda-seiji@ikechan.local>
2023-01-04 22:30:24 +0900

    通貨のカスタムバリデーター追加

--- api/account.go
+++ api/account.go
@@ -11,7 +11,7 @@ import (
 
 type createAccountReqest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR JPY"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createAccount(ctx *gin.Context) {
